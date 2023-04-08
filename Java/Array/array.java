b086d3e8d44144dbb32b2cb8a3f732b2885bb3d6
cyborg3301 <123007668+cyborg3301@users.noreply.github.com>
2023-04-08 22:44:59 +0600

    Basic array code

---
+++ Java/Array/array.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        //This is creating an Array and declaring its size
+       // int[] myArray = new int[10];
+
+        //declaring array with values inline
+        int[] myArray = {1, 2, 3, 4, 5};
+        for(int i = 0; i < myArray.length; i++) {
+            System.out.println(myArray[i]);
+        }
+    }
+}
